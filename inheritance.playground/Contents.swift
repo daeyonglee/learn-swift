0e7cdc0653f63c43625f3c439ef5df1a69176c7c
ldy <dleo238@gmail.com>
2025-08-25 17:03:51 +0900

    feat: inheritance

---
+++ inheritance.playground/Contents.swift
@@ -0,0 +1,36 @@
+import UIKit
+
+
+
+class Friend {
+    var name : String
+    
+    init(_ name: String) {
+        self.name = name
+    }
+    
+    func sayHi() {
+        print("안녕?! 난 \(self.name) 라고 해")
+    }
+}
+
+class BestFriend : Friend {
+    
+    
+    override init(_ name: String) {
+        super.init("베프 \(name)")
+    }
+    
+    override func sayHi() {
+        super.sayHi()
+    }
+}
+
+
+let myFriend = Friend("김철수")
+
+myFriend.sayHi()
+
+let bestFriend = BestFriend("김철수")
+
+bestFriend.sayHi()
