5dfadea3284968634fd83f568ab01c3c559dcfcf
ldy <dleo238@gmail.com>
2025-08-27 13:49:35 +0900

    feat: merge collection

---
+++ merge-collection.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+let friends = ["철수", "영희", "수잔"]
+let otherFriends = ["제임스", "존슨", "존시나"]
+let setsFriends: Set<String> = ["민수", "영철", "존시나"]
+
+let totalFriends = friends + otherFriends + setsFriends
+
+
+print(totalFriends)
